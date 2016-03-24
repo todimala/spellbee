573454be7428474b1fc48e29c6a31046ed9dce4b
sudipto1302 <sudipto1306@gmail.com>
2016-03-24 18:36:00 -0500

    eureka configure

--- WorkflowMicroService/src/main/java/com/citigroup/workflow/WorkflowMicroServiceApplication.java
+++ WorkflowMicroService/src/main/java/com/citigroup/workflow/WorkflowMicroServiceApplication.java
@@ -3,10 +3,13 @@ package com.citigroup.workflow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class WorkflowMicroServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hi Sudipto Here!!!!!!");
 		SpringApplication.run(WorkflowMicroServiceApplication.class, args);
 	}
 }
---
+++ spring-eureka/src/main/java/com/citigroup/eureka/SpringEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.citigroup.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEurekaApplication.class, args);
+	}
+}
